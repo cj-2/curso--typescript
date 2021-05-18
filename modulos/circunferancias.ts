02c3f3a7472e1adf27bb16724ddb5913a8be7aad
Carlos Roberto <crpu4a@gmail.com>
2021-05-18 10:30:01 -0300

    Trabalhando com módulos.

---
+++ modulos/circunferancias.ts
@@ -0,0 +1,5 @@
+export const PI = 3.14
+
+export default function areaCircunferencia(raio: number): number {
+  return PI * Math.pow(raio, 2)
+}
---
+++ modulos/modulos.ts
@@ -0,0 +1,5 @@
+import { areaRetangulo as ret } from './retangulo'
+import areaCircunferencia from './circunferancias'
+
+console.log('ret(15, 20) :>> ', ret(15, 20))
+console.log('areaCircunferencia(18) :>> ', areaCircunferencia(18))
